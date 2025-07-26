a4ed159bba03ee8cb3e9567daf19d7d25dee849a
Ganesh Pawar <wellganesh@gmail.com>
2025-07-26 21:47:38 +0530

    initial commit : TLS , Rep Dir Create, mov certs (#33)
    Co-authored-by: mockarch <arch.gp@yandex.com>

--- app/hydraidectl/cmd/list.go
+++ app/hydraidectl/cmd/list.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -25,5 +26,5 @@ var listCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(restartCmd)
+	rootCmd.AddCommand(listCmd)
 }
